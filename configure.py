1c637992727a72356b049de6d9846d27178705ee
PanJason <pyyjason@gmail.com>
2020-03-21 17:50:59 +0800

    add resources and configure.py

---
+++ configure.py
@@ -0,0 +1,32 @@
+import os
+##Size of screen window
+SCREENSIZE=(500,260)
+
+##Define colors
+RED=(255,0,0)
+BLACK=(0,0,0)
+AZURE=(240,255,255)
+WHITE=(255,255,255)
+MISTYROSE=(255,228,225)
+PALETURQUOISE=(175,238,238)
+PAPAYAWHIP=(255,239,213)
+LIGHTGRAY=(211,211,211)
+GAINSBORO=(230,230,230)
+WHITESMOKE=(245,245,245)
+DARKGRAY=(169,169,169)
+BLUE=(0,0,255)
+DEEPSKYBLUE=(0,191,255)
+SKYBLUE=(135,206,235)
+LIGHTSKYBLUE=(135,206,250)
+
+##Path of Background music
+BGM_PATH =os.path.join(os.getcwd(),"resources/music/Claude Debussy - Clair De Lune.mp3")
+
+##Path of Fonts
+FONT_PATH = os.path.join(os.getcwd(), "resources/font/gb5wb1b.TTF")
+
+##Path of Background image
+BD_IMAGE_PATH=os.path.join(os.getcwd(), "resources/images/bg.png")
+
+##Path of Icon
+ICON_IMAGE_PATH= os.path.join(os.getcwd(),"resources/images/icon.png")
